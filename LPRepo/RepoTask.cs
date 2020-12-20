89aca9a15ad032d8489edb5dc9e3e53899273724
szkmiyabi <led.centipede@gmail.com>
2020-12-20 22:56:07 +0900

    コミット積み残し

--- LPRepo/RepoTask.cs
+++ LPRepo/RepoTask.cs
@@ -45,23 +45,34 @@ namespace LPRepo
             this.Invoke(__write_log, "検査メイン画面に移動します。（" + DateUtil.get_logtime() + "）");
             ldr.browse_sv_mainpage();
             DateUtil.app_sleep(longWait);
-            ldr.select_category("リスト");
-            DateUtil.app_sleep(shortWait);
-            ldr.select_view("検査項目一覧");
-            DateUtil.app_sleep(shortWait);
-            List<List<object>> data = ldr.get_survey_details();
-            for(int i=0; i<data.Count; i++)
-            {
-                var row = data[i];
-                string v1 = (string)row[0];
-                string v2 = (string)row[1];
-                this.Invoke(__write_log, v1 + "  " + v2);
-                List<string> gs = (List<string>)row[2];
-                foreach(string tx in gs)
-                {
-                    this.Invoke(__write_log, tx);
-                }
 
+            this.Invoke(__write_log, "検査カテゴリの取得を開始します。（" + DateUtil.get_logtime() + "）");
+            List<string> categories = ldr.get_category_list_data_from_svpage();
+            int cnx = 0;
+            foreach (string category in categories)
+            {
+                if (cnx == 2) break;
+                this.Invoke(__write_log, "検査カテゴリ：" + category + "の処理をしています...（" + DateUtil.get_logtime() + "）");
+                ldr.select_category(category);
+                DateUtil.app_sleep(midWait);
+                ldr.select_view("検査項目一覧");
+                DateUtil.app_sleep(midWait);
+                List<List<object>> data = ldr.get_survey_details();
+                DateUtil.app_sleep(shortWait);
+                for (int i = 0; i < data.Count; i++)
+                {
+                    var row = data[i];
+                    string v0 = (string)row[0];
+                    string v1 = (string)row[1];
+                    string v2 = (string)row[2];
+                    this.Invoke(__write_log, v0 + "  " + v1 + "  " + v2);
+                    List<string> gs = (List<string>)row[3];
+                    foreach (string tx in gs)
+                    {
+                        this.Invoke(__write_log, tx);
+                    }
+                }
+                cnx++;
             }
             ldr.logout();
             this.Invoke(__write_log, "処理が完了しました。（" + DateUtil.get_logtime() + "）");
